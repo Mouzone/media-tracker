45d73716775686d1114eb6f47f0800ca33f78398
Mouzone <sunnyliu010@gmail.com>
2025-09-24 09:55:10 -0400

    Add Roboto font and create universal stylesheet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,10 @@
-<script>
-	import { enhance } from "$app/forms";
-</script>
-
-<form
-	method="POST"
-	action="?/create"
-	use:enhance
->
-	<label>
-		Title
-		<input
-			name="title"
-			autocomplete="off"
-			required
-		/>
-	</label>
-
-	<label>
-		Media Type
-		<select
-			name="media_type"
-			required
-		>
-			<option value="audio"> Audio </option>
-			<option value="book"> Book </option>
-			<option value="tv_show"> TV Show </option>
-			<option value="movie"> Movie </option>
-		</select>
-	</label>
-
-	<button type="submit"> Submit </button>
-</form>
+<div id="banner">Media Tracker</div>
+<div></div>
 
 <style>
-	form {
-		display: flex;
-		flex-direction: column;
-		gap: 1em;
+	#banner {
+		background-color: chartreuse;
+		font-weight: bold;
+		font-size: larger;
 	}
 </style>
---
+++ src/routes/form.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { enhance } from "$app/forms";
+</script>
+
+<form
+	method="POST"
+	action="?/create"
+	use:enhance
+>
+	<label>
+		Title
+		<input
+			name="title"
+			autocomplete="off"
+			required
+		/>
+	</label>
+
+	<label>
+		Media Type
+		<select
+			name="media_type"
+			required
+		>
+			<option value="audio"> Audio </option>
+			<option value="book"> Book </option>
+			<option value="tv_show"> TV Show </option>
+			<option value="movie"> Movie </option>
+		</select>
+	</label>
+
+	<button type="submit"> Submit </button>
+</form>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+</style>
