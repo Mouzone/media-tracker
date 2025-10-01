7e70487a8a3319888dc9f51fc91c354646c6495d
Mouzone <sunnyliu010@gmail.com>
2025-10-01 14:00:38 -0400

    Style the banner so it is on top of the image

--- src/lib/components/RecordThumbnail.svelte
+++ src/lib/components/RecordThumbnail.svelte
@@ -15,15 +15,32 @@
 <style>
 	li {
 		list-style: none;
-		height: 10em;
-		width: 12em;
+	}
+	button {
+		background-color: white;
+		border: none;
+		position: relative;
+		display: inline-block;
+		line-height: 0;
 	}
 	img {
+		display: block;
 		width: 100%;
+		height: auto;
 	}
 
 	p {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+
 		text-align: center;
 		overflow: hidden;
+		background-color: black;
+		color: white;
+		opacity: 60%;
+		padding: 10px;
+		box-sizing: border-box;
 	}
 </style>
